b12771f25de998ecd56e8c207fc3c739f938affb
HarshaBonagiri <72388172+HarshaBonagiri@users.noreply.github.com>
2020-11-06 14:18:36 +0530

    added optimized code for person details such as first and last name,Id etc

--- person.java
+++ person.java
@@ -1,49 +1,67 @@
-class Person{
-        //int id;
-        String firstName;
-        String lastName;
-        Address address;
+import java.lang.Comparable;
+import java.io.Serializable;
+
+class Person implements Comparable<Person>,Serializable{
+	private static final long serialVersionUID = 1L;
+    //int id;
+    String firstName;
+    String lastName;
+    Address address;
 	String phoneNumber;
 
-	Person(){}
 	Person(String firstName,String lastName,Address address,String phoneNumber){
 		this.firstName=firstName;
 		this.lastName=lastName;
 		this.address=address;
 		this.phoneNumber=phoneNumber;
 	}
+
 	public String getFirstName(){
 		return this.firstName;
 	}
+
 	public void setFirstName(String firstName){
 		this.firstName=firstName;
 	}
+
 	public String getLastName(){
 		return this.lastName;
 	}
+
 	public void setLastName(String lastName){
 		this.lastName=lastName;
 	}
-	public String getAddress(){
-		return this.address.toString();
+
+	public Address getAddress(){
+		return this.address;
 	}
-	public void setAddress(Address address){
-		this.address=address;
+
+	public void setAddress(Address addr){
+		this.address = addr;
 	}
+
 	public String getPhoneNumber(){
 		return this.phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber){
 		this.phoneNumber=phoneNumber;
 	}
+
 	@Override
 	public String toString(){
-		return this.firstName +" "+ this.lastName +"\n"+ this.phoneNumber +"\n"+ this.address.toString() ;
+		return this.firstName +" "+ this.lastName +"-\n"+ this.phoneNumber +"\n"+ this.address.toString() ;
 	}
 
-	/*public static void main(String[] args){
-		Address newAddr = new Address("F45,Hathway Street","Near SBI Bank Borivali","Mumbai","Maharashtra",400066);
-		Person kim = new Person("kim jong","un",newAddr,"9312457120");
-		System.out.println(kim.toString());
-	}*/
+	@Override
+	public int compareTo(Person person){
+		if(this.address.getZip() > person.address.getZip()){
+			return 1;
+		}
+
+		if(this.address.getZip() < person.address.getZip()){
+			return -1;
+		}
+		return 0;
+	}
 }
